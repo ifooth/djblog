7031be275b487a89499956431c59a69cf45f5911
Joe Lei <thezero12@hotmail.com>
2013-07-09 00:17:29 +0800

    添加 mobile app

---
+++ mobile/views.py
@@ -0,0 +1,15 @@
+#encoding=utf-8
+import datetime
+import hashlib
+
+from django.http import HttpResponse,HttpResponseRedirect
+from django.template import Context,loader,RequestContext
+from django.shortcuts import render_to_response
+from blog.models import Post, Page
+
+def index(request):
+    t = loader.get_template('mobile/index.html')
+    c = RequestContext(request)
+    posts = Post.objects.all()
+    c.update({'posts':posts})
+    return HttpResponse(t.render(c))
